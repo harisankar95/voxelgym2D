20b8152c554a10ff5c5aff51653d704b180c5621
Hari <harisankar995@gmail.com>
2023-02-11 16:05:27 +0100

    a small test function just to make sure that things are working

---
+++ tests/test_env.py
@@ -0,0 +1,26 @@
+"""Test the environment."""
+
+import gym
+import numpy as np
+import pytest
+from gym.utils.env_checker import check_env
+
+import voxelgym2D
+
+
+def test_onsestep():
+    """test onestep env"""
+    env = gym.make("voxelgym2D:onestep-v0")
+    check_env(env)
+    _ = env.reset()
+    _, i = env.reset(return_info=True)
+    # assert i is a dict
+    assert isinstance(i, dict)
+
+    del env
+    # test continuous action space
+    env = gym.make(
+        "voxelgym2D:onestep-v0",
+        discrete_actions=False,
+    )
+    check_env(env)
